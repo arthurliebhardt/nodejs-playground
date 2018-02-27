02d3eddd177ae78597d3cdc9ba58f44ebec7e0b7
Arthur Liebhardt <arthur.liebhardt@sap.com>
2018-02-27 23:36:28 +0100

    Added mining of pending transactions and get the balance of an address

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # tiny-js-blockchain-example
 This repo is just a little playground for an nodejs blockchain implementation. So its not best practice, good tested or for productive usage to use :-).
 
-Tutorials:
+Blockchain Tutorials:
 - [x] https://www.savjee.be/2017/07/Writing-tiny-blockchain-in-JavaScript/
 - [x] https://www.savjee.be/2017/09/Implementing-proof-of-work-javascript-blockchain/
+- [x] https://www.savjee.be/2018/02/Transactions-and-mining-rewards/
--- blockchain.js
+++ blockchain.js
@@ -27,8 +27,8 @@ class Block {
 }
 
 class Transaction {
-    constructor(fromAdress, toAddress, amount) {
-        this.fromAdress = fromAdress;
+    constructor(fromAddress, toAddress, amount) {
+        this.fromAddress = fromAddress;
         this.toAddress = toAddress;
         this.amount = amount;
     }
@@ -37,7 +37,13 @@ class Transaction {
 class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
-        this.difficulty = 5;
+        this.difficulty = 2;
+
+        // Place to store transactions in between block creation
+        this.pendingTransactions = [];
+
+        // How many coins a miner will get as a reward for his/her efforts
+        this.miningReward = 100;
     }
 
     createGenesisBlock() {
@@ -48,11 +54,53 @@ class Blockchain {
         return this.chain[this.chain.length - 1];
     }
 
-    addBlock(newBlock) {
-        newBlock.previousHash = this.getLatestBlock().hash;
-        // newBlock.hash = newBlock.calculateHash();
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
+    // addBlock(newBlock) {
+    //     newBlock.previousHash = this.getLatestBlock().hash;
+    //     // newBlock.hash = newBlock.calculateHash();
+    //     newBlock.mineBlock(this.difficulty);
+    //     this.chain.push(newBlock);
+    // }
+    createTransaction(transaction) {
+        // There should be some validation here!
+
+        // Push into the "pendinTransactions" array
+        this.pendingTransactions.push(transaction);
+    }
+
+    minePendingTransactions(miningRewardAddress) {
+        // Create new block with all pending transactions and mine it..
+        let block = new Block(Date.now(), this.pendingTransactions);
+        block.mineBlock(this.difficulty);
+
+        // Add the newly mined block to the chain
+        this.chain.push(block);
+
+        // Reset the pending transactions and send the mining reward
+        this.pendingTransactions = [
+            new Transaction(null, miningRewardAddress, this.miningReward)
+        ];
+    }
+
+    getBalanceOfAddress(address) {
+        let balance = 0; // you start at zero!
+
+        // Loop over each block and each transaction inside the block
+        for(const block of this.chain) {
+            for(const trans of block.transactions) {
+
+                // if the given address is the sender -> reduce the balance
+                if(trans.fromAddress === address) {
+                    balance -= trans.amount;
+                }
+
+                // If the given address is the receiver -> increase the balance
+                if(trans.toAddress === address) {
+                    balance += trans.amount;
+                }
+            }
+        }
+
+        return balance;
     }
 
     isChainValid() {
@@ -78,20 +126,34 @@ class Blockchain {
 
 let atrChain = new Blockchain();
 
-console.log('Mining block 1');
-atrChain.addBlock(new Block(1, "27/01/2018", { amount: 4}));
+console.log('Creating some transactions...');
+atrChain.createTransaction(new Transaction('address1', 'address2', 100));
+atrChain.createTransaction(new Transaction('address2', 'address1', 50));
 
-console.log('Mining block 2');
-atrChain.addBlock(new Block(2, "27/01/2018", { amount: 8}));
+console.log('Starting the miner...');
+atrChain.minePendingTransactions('atr-address');
 
-// Check if chain is valid (will return true)
-console.log('Blockchain valid? ' + atrChain.isChainValid());
+console.log(`Balance of atrs address is ${atrChain.getBalanceOfAddress('atr-address')}` )
 
-// Let's now manipulate the data
-console.log('Changing a block...');
-atrChain.chain[1].data = { amount: 100 };
+console.log('Starting the miner again!');
+atrChain.minePendingTransactions('atr-address');
+console.log(`Balance of atrs address is ${atrChain.getBalanceOfAddress('atr-address')}` )
 
-// Check our chain again (will now return false)
-console.log('Blockchain valid? ' + atrChain.isChainValid());
+// from version 1 and 2
+// console.log('Mining block 1');
+// atrChain.addBlock(new Block(1, "27/01/2018", { amount: 4}));
 
-console.log(JSON.stringify(atrChain, null, 4));
+// console.log('Mining block 2');
+// atrChain.addBlock(new Block(2, "27/01/2018", { amount: 8}));
+
+// // Check if chain is valid (will return true)
+// console.log('Blockchain valid? ' + atrChain.isChainValid());
+
+// // Let's now manipulate the data
+// console.log('Changing a block...');
+// atrChain.chain[1].data = { amount: 100 };
+
+// // Check our chain again (will now return false)
+// console.log('Blockchain valid? ' + atrChain.isChainValid());
+
+// console.log(JSON.stringify(atrChain, null, 4));
